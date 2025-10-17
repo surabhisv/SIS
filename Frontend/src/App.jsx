dc5dd21225216e2cbc641849e0215ee529764920
Santhosh A <santhosh737mc@gmail.com>
2025-10-17 22:41:31 +0530

    Added student pages and the corresponding routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,45 @@
-import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import Home from './Pages/Home.jsx';
-import SuperAdmin from './Pages/SuperAdmin/SuperAdmin.jsx';
-import SuperAdminPortal from './Pages/SuperAdmin/SuperAdminPortal.jsx';
-import StudentLogin from './Pages/Student/StudentLogin.jsx';
-import AdminLogin from './Pages/Admin/AdminLogin.jsx';
-import StudentRegister from './Pages/Student/StudentRegister.jsx';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// Page components
+import Home from "./Pages/Home.jsx";
+
+import SuperAdmin from "./Pages/SuperAdmin/SuperAdmin.jsx";
+import SuperAdminPortal from "./Pages/SuperAdmin/SuperAdminPortal.jsx";
+
+import StudentRegister from "./Pages/Student/StudentRegister.jsx";
+import StudentLogin from "./Pages/Student/StudentLogin.jsx";
+import StudentDashboard from "./pages/student/StudentDashboard";
+import StudentProfile from "./pages/student/StudentProfile";
+import BrowseCourses from "./pages/student/BrowseCourses";
+import StudentEnrollments from "./pages/student/StudentEnrollments";
+
+import AdminLogin from "./Pages/Admin/AdminLogin.jsx";
 
 function App() {
- return(
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<Home/>}/>  {/*home page navigation*/} 
-    <Route path='/superadmin' element={<SuperAdmin/>}/>    {/*superadmin login page navigation*/}
-    <Route path='/superadmin/portal' element={<SuperAdminPortal/>}/> {/*superadmin portal page navigation*/}
-    <Route path='/student/Studentlogin' element={<StudentLogin/>}/> {/*student login page navigation*/}
-    <Route path='/admin/Adminlogin' element={<AdminLogin/>}/> {/*admin login page navigation*/}
-    <Route path='/student/register' element={<StudentRegister/>}/> {/*student registration page navigation*/}
-   
-  </Routes>
-  </BrowserRouter>
- )
+  return (
+    <BrowserRouter>
+      {/* App routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        {/* Superadmin pages */}
+        <Route path="/superadmin" element={<SuperAdmin />} />
+        <Route path="/superadmin/portal" element={<SuperAdminPortal />} />
+
+        {/* Student pages */}
+        <Route path="/student/Studentlogin" element={<StudentLogin />} />
+        <Route path="/student/register" element={<StudentRegister />} />
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
+        <Route path="/student/profile" element={<StudentProfile />} />
+        <Route path="/student/courses" element={<BrowseCourses />} />
+        <Route path="/student/enrollments" element={<StudentEnrollments />} />
+
+        {/* Admin pages */}
+        <Route path="/admin/Adminlogin" element={<AdminLogin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
